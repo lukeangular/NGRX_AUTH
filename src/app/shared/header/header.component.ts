9c6a7181e8c6aff91b6e39c0d5ba8ca91d3fc4ad
sundartamang <sundartamang6@gmail.com>
2023-09-27 15:51:05 +0545

    fix:hide logi when user logged in

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppSate } from 'src/app/appstate/app.state';
+import { isUserAuthenticated } from 'src/app/auth/state/auth.selector';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  isAuthenticated: Observable<boolean>
+  constructor(
+    private _store: Store<AppSate>
+  ){}
+
+  ngOnInit(): void {
+    this.isAuthenticated = this._store.select(isUserAuthenticated)
+  }
 }
